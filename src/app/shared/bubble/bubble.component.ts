15d1173b6992f14689f55afd4f45ad2550dc0780
SergeiKrivko <sergik.krivko@gmail.com>
2024-09-27 18:22:00 +0300

    Пофикшен latex

--- src/app/shared/bubble/bubble.component.ts
+++ src/app/shared/bubble/bubble.component.ts
@@ -110,15 +110,13 @@ export class BubbleComponent implements OnInit {
    delimiters: [
      {left: "$$", right: "$$", display: true},
      {left: '$', right: '$', display: false}, // I added
-     {left: "\\(", right: "\\)", display: false},
-     {left: "\(", right: "\)", display: false}, // I added
+     {left: "\\\(", right: "\\\)", display: false},
      {left: "\\begin{equation}", right: "\\end{equation}", display: true},
      {left: "\\begin{align}", right: "\\end{align}", display: true},
      {left: "\\begin{alignat}", right: "\\end{alignat}", display: true},
      {left: "\\begin{gather}", right: "\\end{gather}", display: true},
      {left: "\\begin{CD}", right: "\\end{CD}", display: true},
-     {left: "\\[", right: "\\]", display: true},
-     {left: "\[", right: "\]", display: true}, // I added
+     {left: "\\\[", right: "\\\]", display: true},
    ]
  };
 
