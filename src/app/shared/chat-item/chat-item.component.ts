37a75b1ed19dcca9e75d526d60dff777c2a89f5e
SergeiKrivko <sergik.krivko@gmail.com>
2024-09-30 12:22:25 +0300

    Последние сообщения

--- src/app/shared/chat-item/chat-item.component.ts
+++ src/app/shared/chat-item/chat-item.component.ts
@@ -1,9 +1,11 @@
-import {Component, inject, Input} from '@angular/core';
+import {Component, inject, Input, OnInit} from '@angular/core';
 import {IonicModule} from "@ionic/angular";
 import {RouterLink} from "@angular/router";
 import {NgIf} from "@angular/common";
 import {Chat} from "../../core/models/chat";
 import {ChatsService} from "../../core/services/chats.service";
+import {map, tap} from "rxjs";
+import {Message} from "../../core/models/message";
 
 @Component({
   selector: 'app-chat-item',
@@ -16,10 +18,20 @@ import {ChatsService} from "../../core/services/chats.service";
   templateUrl: './chat-item.component.html',
   styleUrl: './chat-item.component.scss'
 })
-export class ChatItemComponent {
+export class ChatItemComponent implements OnInit {
   @Input() chat?: Chat
 
   private readonly chatsService = inject(ChatsService);
+  protected lastMessage: Message | null = null;
+
+  ngOnInit() {
+    if (this.chat) {
+      this.chatsService.getMessages(this.chat.uuid).pipe(
+        map(messages => messages.length ? messages[0] : null),
+        tap(m => this.lastMessage = m)
+      ).subscribe()
+    }
+  }
 
   public chatLink(): string {
     return this.chat ? `/chat/${this.chat.uuid}` : ''
